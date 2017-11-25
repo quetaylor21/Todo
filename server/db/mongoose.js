a93ba2e2aff508ba6d1692a83a51cebd0018ba30
Marcquin Taylor <Taylor@Quins-iMac.attlocal.net>
2017-11-25 02:01:59 -0500

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@
 
   // let mongoose know you want to us promises instead of callbacks
   mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost:27017/TodoApp');
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
   module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@
     var {User} = require('./models/user');
 
     var app = express();
+    const port = process.env.PORT || 3000;
 
     app.use(bodyParser.json());
 
@@ -54,8 +55,8 @@
         res.status(400).send()
       })
     })
-    app.listen(8060, () => {
-      console.log('Started on port 8060')
+    app.listen(port, () => {
+      console.log(`Started on port ${port}`)
     });
 
     module.exports = {app}
