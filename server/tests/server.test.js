42b69ebba9bbe60e7eddd833919391f6a01265d2
Marcquin Taylor <Taylor@Quins-iMac.attlocal.net>
2017-11-25 22:14:54 -0500

    test DELETE functionality

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -121,4 +121,43 @@
 
         })
 
+        describe('DELETE /todos:id', () => {
+
+          it("Should remove a todo", (done) => {
+            var hexId = todosList[0]._id.toHexString()
+            request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+              expect(res.body.todo._id).toBe(hexId)
+            })
+            .end((err, res) => {
+              if(err){
+                return done(err)
+              }
+              Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist()
+                done()
+              }).catch((e) => {
+                done(e)
+              })
+            })
+          })
+
+          it("Should return 404 if todo is not found", (done) => {
+            var hexId = new ObjectID().toHexString();
+            request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done)
+          })
+
+          it("Should return 404 if objectID is invalid", (done) => {
+            request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done)
+          })
+        })
+
       });
